56d9c6c69700f4f720cbb03c3c29cad6cb3d7a82
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-02-20 16:49:41 +0000

    * factored colorspace manipulation out of optimize2bw into lib/Colorspace*

--- bin/optimize2bw.cc
+++ bin/optimize2bw.cc
@@ -26,6 +26,8 @@
 #include "jpeg.hh"
 #include "Image.hh"
 
+#include "Colorspace.hh"
+
 using namespace Utility;
 
 int main (int argc, char* argv[])
@@ -94,17 +96,7 @@ int main (int argc, char* argv[])
   if (image.spp == 3 && image.bps == 8) {
     std::cerr << "RGB -> Gray convertion" << std::endl;
     
-    unsigned char* output = image.data;
-    for (unsigned char* it = image.data; it < image.data + image.w*image.h*image.spp;)
-      {
-	// R G B order and associated weighting
-	int c = (int)*it++ * 28;
-	c += (int)*it++ * 59;
-	c += (int)*it++ * 11;
-	
-	*output++ = (unsigned char)(c / 100);
-      }
-    image.spp = 1; // converted data right now
+    colorspace_rgb_to_gray (image);
   }
   else if (image.spp != 1 && image.bps != 8)
     {
@@ -112,47 +104,20 @@ int main (int argc, char* argv[])
 		<< image.bps << " bits per sample." << std::endl;
       return 1;
     }
+ 
+  int lowest = 0, highest = 0;
   
-  {
-    int histogram[256] = { 0 };
-    for (unsigned char* it = image.data; it < image.data + image.w*image.h;)
-      histogram[*it++]++;
-
-    int lowest = 255, highest = 0;
-    for (int i = 0; i <= 255; i++)
-      {
-	// std::cout << i << ": "<< histogram[i] << std::endl;
-	if (histogram[i] > 2) // magic denoise constant
-	  {
-	    if (i < lowest)
-	      lowest = i;
-	    if (i > highest)
-	      highest = i;
-	  }
-      }
-    std::cerr << "lowest: " << lowest << " - highest: "
-	      << highest << std::endl;
-    
-    if (arg_low.Get() != 0) {
-      lowest = arg_low.Get();
-      std::cerr << "Low value overwritten: " << lowest << std::endl;
-    }
-    
-    if (arg_high.Get() != 0) {
-      highest = arg_high.Get();
-      std::cerr << "High value overwritten: " << highest << std::endl;
-    }
-    
-    // TODO use options
-    signed int a = (255 * 256) / (highest - lowest);
-    signed int b = -a * lowest;
-
-    std::cerr << "a: " << (float) a / 256
-	      << " b: " << (float) b / 256 << std::endl;
-
-   for (unsigned char* it = image.data; it < image.data + image.w*image.h; ++it)
-     *it = ((int) *it * a + b) / 256;
+  if (arg_low.Get() != 0) {
+    lowest = arg_low.Get();
+    std::cerr << "Low value overwritten: " << lowest << std::endl;
   }
+  
+  if (arg_high.Get() != 0) {
+    highest = arg_high.Get();
+    std::cerr << "High value overwritten: " << highest << std::endl;
+  }
+  
+  normalize (image, lowest, highest);
   
   // Convolution Matrix (unsharp mask a-like)
   unsigned char *data2 = (unsigned char *) malloc (image.w * image.h);
@@ -334,9 +299,6 @@ int main (int argc, char* argv[])
   
   // convert to 1-bit (threshold)
   
-  unsigned char *output = image.data;
-  unsigned char *input = image.data;
-  
   int threshold = 170;
     
   if (arg_threshold.Get() != 0) {
@@ -344,32 +306,7 @@ int main (int argc, char* argv[])
     std::cerr << "Threshold: " << threshold << std::endl;
   }
     
-  for (int row = 0; row < image.h; row++)
-
-    {
-      unsigned char z = 0;
-      int x = 0;
-      for (; x < image.w; x++)
-	{
-	  z <<= 1;
-	  if (*input++ > threshold)
-	    z |= 0x01;
-	  
-	  if (x % 8 == 7)
-	    {
-	      *output++ = z;
-	      z = 0;
-	    }
-	}
-      // remainder - TODO: test for correctness ...
-      int remainder = 8 - x % 8;
-      if (remainder != 8)
-	{
-	  z <<= remainder;
-	  *output++ = z;
-	}
-    }
-  image.bps = 1;
+  colorspace_gray_to_bilevel (image, threshold);
 
   if (!image.Write(arg_output.Get())) {
     std::cerr << "Error writing output file." << std::endl;
