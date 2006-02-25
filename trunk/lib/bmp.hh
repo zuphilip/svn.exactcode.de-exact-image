e4e74e3584c152969ea544c99bd5129d938ddf51
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-02-25 17:38:17 +0000

    * fixed typo in lib/bmp.hh and added the bmp functions to the 	  Image Read/Write multiplexer

--- trunk/lib/bmp.hh
+++ trunk/lib/bmp.hh
@@ -5,5 +5,5 @@ read_BMP_file (const char* file, int* w, int* h, int* bps, int* spp,
                int* xres, int* yres);
 
 void
-write_BMPfile (const char* file, unsigned char* data, int w, int h, int bps, int spp,
+write_BMP_file (const char* file, unsigned char* data, int w, int h, int bps, int spp,
                 int xres, int yres);
