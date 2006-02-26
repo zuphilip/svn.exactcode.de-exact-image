17f566c1ca2d9aac7a07749ea3de82ba7f3f4249
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-02-26 10:53:23 +0000

    * finalized extern "C", include bmp.h in bmp.c for checking, factored 	  1bps inverted table deteciton out, pass the color table back and 	  skip inverted and corrected color tables

--- trunk/lib/bmp.cc
+++ trunk/lib/bmp.cc
@@ -1,13 +1,27 @@
 
-#include "bmp.h"
+#include <iostream>
 
+#include "bmp.h"
 #include "bmp.hh"
 
 unsigned char*
 read_BMP_file (const char* file, int* w, int* h, int* bps, int* spp,
                int* xres, int* yres)
 {
-  return read_bmp (file, w, h, bps, spp, xres, yres);
+  unsigned char* color_table = 0;
+  unsigned char* data = read_bmp (file, w, h, bps, spp,
+				  xres, yres, &color_table);
+  
+  if (color_table != 0 && *spp < 3 && *bps <= 8) {
+    /* convert to RGB color-space - we do not handle palet images internally */
+
+    // TODO: detect gray tables
+    // TODO: detect correct 1bps b/w tables ?
+    
+    std::cerr << "TODO: convert to RGB" << std::endl;
+  }
+  
+  return data;
 }
 
 void
--- trunk/libbmp/bmp.h
+++ trunk/libbmp/bmp.h
@@ -1,6 +1,12 @@
 
+
+#ifdef __cplusplus
 extern "C" {
-  
-  unsigned char* read_bmp (const char* file, int* w, int* h, int* bps, int* spp,
-			   int* xres, int* yres);
+#endif
+
+unsigned char* read_bmp (const char* file, int* w, int* h, int* bps, int* spp,
+			 int* xres, int* yres, unsigned char** color_table);
+
+#ifdef __cplusplus
 }
+#endif
