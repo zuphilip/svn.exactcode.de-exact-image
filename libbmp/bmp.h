83f3f28cceaee2909445822ad2e8c69c66d6ff5a
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-02-26 21:29:16 +0000

    * export more color table details in libbmp 	* convert BMP palete data to RGB colorspace in the exact-image code

--- libbmp/bmp.h
+++ libbmp/bmp.h
@@ -5,7 +5,8 @@ extern "C" {
 #endif
 
 unsigned char* read_bmp (const char* file, int* w, int* h, int* bps, int* spp,
-			 int* xres, int* yres, unsigned char** color_table);
+			 int* xres, int* yres, unsigned char** color_table,
+			 int* color_table_size, int* color_table_elements);
 
 #ifdef __cplusplus
 }
