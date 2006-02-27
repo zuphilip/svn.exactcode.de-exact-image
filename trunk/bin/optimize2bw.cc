6128f7e156e36878ab25fbc8ac07a4bfcb00b2b7
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-02-27 18:04:39 +0000

    * fixed .hh ending of some headers 	* let optimize2bw use the factored out linear_scale 	* added --scale using linear_scale to econvert

--- trunk/bin/optimize2bw.cc
+++ trunk/bin/optimize2bw.cc
@@ -28,6 +28,7 @@
 
 #include "Colorspace.hh"
 #include "Matrix.hh"
+#include "scale.hh"
 
 using namespace Utility;
 
@@ -203,54 +204,7 @@ int main (int argc, char* argv[])
   std::cerr << "Scale: " << scale << std::endl;
   
   if (scale > 0.0) {
-
-    int wn = (int) (scale * (double) image.w);
-    int hn = (int) (scale * (double) image.h);
-
-    image.xres = (int) (scale * image.xres);
-    image.yres = (int) (scale * image.yres);
-
-    scale = 256.0 / scale;
-
-    std::cerr << "new dimensions: " << wn << " x " << hn 
-	      << " (xres: " << image.xres
-	      << ", yres: " << image.yres << ")" << std::endl;
-
-    unsigned char* ndata = (unsigned char*) malloc (wn * hn);
-
-    unsigned int offset = 0;
-    for (int y=0; y < hn; y++)
-      for (int x=0; x < wn; x++) {
-
-	int bx = (int) (((double) x) * scale);
-	int by = (int) (((double) y) * scale);
-	
-	int sx = std::min(bx / 256, image.w - 1);
-	int sy = std::min(by / 256, image.h - 1);
-	int sxx = std::min(sx + 1, image.w - 1);
-	int syy = std::min(sy + 1, image.h - 1);
-
-	int fxx = bx % 256;
-	int fyy = by % 256;
-	int fx = 256 - fxx;
-	int fy = 256 - fyy;
- 
-	unsigned int value
-	  = fx  * fy  * ( (unsigned int) image.data [sx  + image.w * sy ])
-	  + fxx * fy  * ( (unsigned int) image.data [sxx + image.w * sy ])
-	  + fx  * fyy * ( (unsigned int) image.data [sx  + image.w * syy])
-	  + fxx * fyy * ( (unsigned int) image.data [sxx + image.w * syy]);
-
-	value /= 256 * 256;
-	value = std::min (value, (unsigned int) 255);
-
-	ndata[offset++] = (unsigned char) value;
-      }
-    
-    free (image.data);
-    image.data = ndata;
-    image.w = wn;
-    image.h = hn;
+    linear_scale (image, scale);
   }
   
   // convert to 1-bit (threshold)
