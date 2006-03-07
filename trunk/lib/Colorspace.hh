f0a457cf8fe13a0ca3776f2dd8b519bcb611957e
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-03-07 09:19:49 +0000

    * implemended some more quick 'n dirty colorspacing for edisplay

--- trunk/lib/Colorspace.hh
+++ trunk/lib/Colorspace.hh
@@ -3,3 +3,6 @@ void normalize (Image& image, unsigned char low = 0, unsigned char high = 0);
 
 void colorspace_rgb_to_gray (Image& image);
 void colorspace_gray_to_bilevel (Image& image, unsigned char threshold = 127);
+
+void colorspace_gray_to_rgb (Image& image);
+void colorspace_bilevel_to_gray (Image& image);
