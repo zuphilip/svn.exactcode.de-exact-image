3081774af8405449667058d33c63edff89d289f7
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-04-21 16:38:45 +0000

    * tiny fixes and improvements in the scaler, do be reworked now, anyway

--- trunk/lib/scale.cc
+++ trunk/lib/scale.cc
@@ -37,31 +37,31 @@ void linear_scale (Image& image, double scale)
   new_image.xres = (int) (scale * image.xres);
   new_image.yres = (int) (scale * image.yres);
 
-  scale = 256.0 / scale;
-  
   Image::iterator dst = new_image.begin();
   Image::iterator src = image.begin();
-  for (int y=0; y < new_image.h; ++y)
-    for (int x=0; x < new_image.w; ++x) {
-      int bx = (int) (((double) x) * scale);
-      int by = (int) (((double) y) * scale);
-      
-      int sx = std::min(bx / 256, image.w - 1);
-      int sy = std::min(by / 256, image.h - 1);
-      int sxx = std::min(sx + 1, image.w - 1);
-      int syy = std::min(sy + 1, image.h - 1);
+  for (int y=0; y < new_image.h; ++y) {
+    int sy = std::min((int) ((double)y / scale), image.h - 1);
+    int syy = std::min((int) (((double)y+.5) / scale +.5), image.h - 1);
+    int fy = 256 / (syy - sy);
+    int fyy = 256 - fy;
 
-      int fxx = bx % 256;
-      int fyy = by % 256;
-      int fx = 256 - fxx;
-      int fy = 256 - fyy;
+    for (int x=0; x < new_image.w; ++x) {
+      int sx = std::min((int) ((double)x / scale), image.w - 1);
+      int sxx = std::min((int) (((double)x+.5) / scale +.5), image.w - 1);
+      int fx = 256 / (sxx - sx);
+      int fxx = 256 - fx;
+
+//      std::cout << "sx: " << sx << ", sy: " << sy << ", sxx: " << sxx << ", syy: " << syy << std::endl;
+//      std::cout << "fx: " << fx << ", fy: " << fy << ", fxx: " << fxx << ", fyy: " << fyy << std::endl;
       
       dst.set ( (src.at (sx,  sy ) * fx  * fy +
 		 src.at (sxx, sy ) * fxx * fy +
 		 src.at (sx,  syy) * fx  * fyy +
 		 src.at (sxx, syy) * fxx * fyy) / (256 * 256) );
+//      std::cout << "x: " << x << ", y: " << y << ": " << (int) dst.ptr->gray << std::endl;
       ++dst;
     }
+  }
   
   image = new_image;
   new_image.data = 0;
