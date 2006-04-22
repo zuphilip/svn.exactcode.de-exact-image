341bfa396ecc99944a3f2d195f3828fde9608c93
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-04-22 14:16:39 +0000

    * implemented gray8_to_gray2 (appears to work with the png backend)

--- lib/Colorspace.hh
+++ lib/Colorspace.hh
@@ -4,6 +4,8 @@ void normalize (Image& image, unsigned char low = 0, unsigned char high = 0);
 void colorspace_rgb_to_gray (Image& image);
 void colorspace_gray_to_bilevel (Image& image, unsigned char threshold = 127);
 
+void colorspace_gray8_to_gray2 (Image& imagey);
+
 void colorspace_gray_to_rgb (Image& image);
 void colorspace_bilevel_to_gray (Image& image);
 
