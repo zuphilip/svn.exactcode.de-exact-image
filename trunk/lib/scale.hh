501f671434226479ae9acd6bb791bc7b76351fd1
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-04-22 07:43:29 +0000

    * some unfinished scaling work

--- trunk/lib/scale.hh
+++ trunk/lib/scale.hh
@@ -1,4 +1,5 @@
 
 
+void bilinear_scale (Image& image, double scale);
 void nearest_scale (Image& image, double scale);
-void linear_scale (Image& image, double scale);
+void box_scale (Image& image, double scale);
