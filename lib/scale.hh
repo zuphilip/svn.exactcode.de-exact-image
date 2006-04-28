d422b319a79e0b92dda68abe917d6506729f443d
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-04-28 16:47:55 +0000

    * removed directional_average prototype 	* improved bilinear scaling for low-pixel count and speed 	* ddt debugging on the way

--- lib/scale.hh
+++ lib/scale.hh
@@ -3,8 +3,6 @@
 void nearest_scale (Image& image, double xscale, double yscale);
 void box_scale (Image& image, double xscale, double yscale);
 
-void directional_average_scale_2x (Image& image);
-
 void bilinear_scale (Image& image, double xscale, double yscale);
 void bicubic_scale (Image& image, double xscale, double yscale);
 
