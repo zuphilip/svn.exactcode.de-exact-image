e06d3ee3bdfe62f328bff4b93cc9b2a2c357d6e8
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-05-10 15:31:28 +0000

    * added a grayX_to_rgb function to the Colorspace file, using the 	  Image::iterator, tested for 1 and 2 bit files

--- trunk/lib/Colorspace.hh
+++ trunk/lib/Colorspace.hh
@@ -4,13 +4,14 @@ void normalize (Image& image, unsigned char low = 0, unsigned char high = 0);
 void colorspace_rgb8_to_gray8 (Image& image);
 
 void colorspace_gray8_to_gray1 (Image& image, unsigned char threshold = 127);
-void colorspace_gray8_to_gray2 (Image& imagey);
-void colorspace_gray8_to_gray4 (Image& imagey);
+void colorspace_gray8_to_gray2 (Image& image);
+void colorspace_gray8_to_gray4 (Image& image);
 void colorspace_gray8_to_rgb8 (Image& image);
 
+void colorspace_grayX_to_rgb8 (Image& image);
 
-void colorspace_gray1_to_gray2 (Image& imagey);
-void colorspace_gray1_to_gray4 (Image& imagey);
+void colorspace_gray1_to_gray2 (Image& image);
+void colorspace_gray1_to_gray4 (Image& image);
 void colorspace_gray1_to_gray8 (Image& image);
 
 void colorspace_16_to_8 (Image& image);
