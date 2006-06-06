c90921df47c17ab1c2426e609fa5793280597a82
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-06-06 09:28:13 +0000

    * added all digital camera raw extensions I could find on the internet 	  thru google to the dcraw loader

--- trunk/lib/dcraw.hh
+++ trunk/lib/dcraw.hh
@@ -4,7 +4,20 @@
 class DCRAWLoader : public ImageLoader {
 public:
   
-  DCRAWLoader () { registerLoader ("cr2", this); };
+  DCRAWLoader () {
+    registerLoader ("crw", this); // Canon
+    registerLoader ("cr2", this);
+    registerLoader ("mrw", this); // Mniolta
+    registerLoader ("nef", this); // Nikon
+    registerLoader ("orf", this); // Olympus
+    registerLoader ("raf", this); // Fuji
+    registerLoader ("pef", this); // Pentax
+    registerLoader ("x3f", this); // Sigma
+    registerLoader ("dcr", this); // Kodak
+    registerLoader ("kdc", this); 
+    registerLoader ("srf", this); // Sony
+    // registerLoader ("raw", this); // Panasonic, Casio, Leica, conflict with real "raw" handler :-(
+  };
   virtual ~DCRAWLoader () { unregisterLoader (this); };
 
   virtual bool readImage (FILE* file, Image& image);
