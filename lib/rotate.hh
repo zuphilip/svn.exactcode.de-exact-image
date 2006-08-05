ef7bded0428674d8377fe2b10da1d9b6f6d672cb
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-08-05 20:59:08 +0000

    * background parameter for rotate.hh, likewise

--- lib/rotate.hh
+++ lib/rotate.hh
@@ -4,4 +4,4 @@ void flipX (Image& image);
 void flipY (Image& image);
 
 void shear (Image& image, double xangle, double yangle);
-void rotate (Image& image, double angle);
+void rotate (Image& image, double angle, Image::iterator background);
