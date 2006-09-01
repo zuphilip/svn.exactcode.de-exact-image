965d9308568a8fc73de84c64efbdbf455f1bf10b
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-09-01 14:43:13 +0000

    * copied 0.0.16 to 0.0.17 to fix segfault

---
+++ tags/0.0.17/lib/dcraw.hh
@@ -0,0 +1,26 @@
+
+#include "ImageLoader.hh"
+
+class DCRAWLoader : public ImageLoader {
+public:
+  
+  DCRAWLoader () {
+    registerLoader ("dcraw", this); // primary entry
+    registerLoader ("crw", this); // Canon
+    registerLoader ("cr2", this);
+    registerLoader ("mrw", this); // Mniolta
+    registerLoader ("nef", this); // Nikon
+    registerLoader ("orf", this); // Olympus
+    registerLoader ("raf", this); // Fuji
+    registerLoader ("pef", this); // Pentax
+    registerLoader ("x3f", this); // Sigma
+    registerLoader ("dcr", this); // Kodak
+    registerLoader ("kdc", this); 
+    registerLoader ("srf", this); // Sony
+    registerLoader ("raw", this); // Panasonic, Casio, Leica
+  };
+  virtual ~DCRAWLoader () { unregisterLoader (this); };
+
+  virtual bool readImage (FILE* file, Image& image);
+  virtual bool writeImage (FILE* file, Image& image, int quality, const std::string& compress);
+};
