4558e5029e37409148153dda4c3bd14484f62a10
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-09-02 20:32:26 +0000

    * first working api bits

--- trunk/api/api.cc
+++ trunk/api/api.cc
@@ -1,21 +1,44 @@
-#include <api.hh>
 
+#include <string>
+#include <sstream>
 #include <iostream>
 
 #include <Image.hh>
+#include <Codecs.hh>
+
+#include <api.hh>
 
 Image* decodeImage (const char* data, int n)
 {
-	std::cout << "here" << std::endl;
+  const std::string str (data, n); 
+  std::istringstream stream (str);
+  
+  Image* image = new Image;
+  if (ImageCodec::Read (&stream, *image))
+    return image;
+  
+  delete image;
+  return 0;
 }
 
-char* encodeImage (Image* image)
+char* encodeImage (Image* image, const char* codec, int quality, const char* compression)
 {
+  /*
+    std::string str ();
+  std::ostringstream stream (str);
+  
+  ImageCodec::Write (&stream, *image, codec, ""; quality, compression);
+  
+  return "";
+  */
 }
 
 Image* decodeImageFile (const char* filename)
 {
-        std::cout << "here" << std::endl;
+  Image* image = new Image;
+  ImageCodec::Read (filename, *image);
+  
+  return image;
 }
 
 bool encodeImageFile (Image* image, const char* filename)
