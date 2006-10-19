982adbb910e93494ba2531b6587df29ae07d3107
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2006-10-19 07:47:47 +0000

    * tagged 0.2.4

---
+++ tags/0.2.4/codecs/dcraw.cc
@@ -0,0 +1,125 @@
+
+#include "dcraw.hh"
+#include <istream>
+#include <sstream>
+
+// now this is an macro conversion from C FILE* to C++ iostream ,-)
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define FILE std::iostream
+
+#undef SEEK_SET
+#undef SEEK_CUR
+#undef SEEK_END
+
+#define SEEK_SET std::ios::beg
+#define SEEK_CUR std::ios::cur
+#define SEEK_END std::ios::end
+
+#define fseek(stream,pos,kind) stream->seekg (pos, kind)
+#define ftell(stream) (int) stream->tellg ()
+
+#define fread(mem,n,m,stream) stream->read ((char*)mem,n*m) ? n*m : 0
+#define fwrite(mem,n,m,stream) stream->write ((char*)mem,n*m) . good() ? n*m : 0
+
+#define fgetc(stream) stream->get ()
+#define fgets(mem,n,stream) stream->get ((char*)mem, n);
+#define putc(c,stream) stream->put (c)
+
+#define tmpfile new std::stringstream
+#define fclose(stream) delete (stream);
+
+#include "dcraw.h"
+
+bool DCRAWCodec::readImage (std::istream* stream, Image& im)
+{
+#ifndef NO_LCMS
+  char *cam_profile = NULL, *out_profile = NULL;
+#endif
+  
+  std::iostream ios (stream->rdbuf());
+  ifp = &ios;
+  
+  identify();
+  
+  if (!is_raw)
+    return false;
+  
+  if (load_raw == kodak_ycbcr_load_raw) {
+    height += height & 1;
+    width  += width  & 1;
+  }
+  
+  int half_size = 0, quality, use_fuji_rotate=1;
+  shrink = half_size && filters;
+  iheight = (height + shrink) >> shrink;
+  iwidth  = (width  + shrink) >> shrink;
+   
+  image = (ushort (*)[4]) calloc (iheight*iwidth*sizeof *image + meta_length, 1);
+  merror (image, "main()");
+  meta_data = (char *) (image + iheight*iwidth);
+  
+  fseek (ifp, data_offset, SEEK_SET);
+  (*load_raw)();
+  // bad_pixels();
+  
+  height = iheight;
+  width  = iwidth;
+  quality = 2 + !fuji_width;
+  
+  if (is_foveon && !document_mode) foveon_interpolate();
+  if (!is_foveon && document_mode < 2) scale_colors();
+  if (shrink) filters = 0;
+  cam_to_cielab (NULL,NULL);
+  if (filters && !document_mode) {
+    if (quality == 0)
+      lin_interpolate();
+    else if (quality < 3 || colors > 3)
+      vng_interpolate();
+    else ahd_interpolate();
+  }
+  if (sigma_d > 0 && sigma_r > 0) bilateral_filter();
+  if (use_fuji_rotate) fuji_rotate();
+#ifndef NO_LCMS
+  if (cam_profile) apply_profile (cam_profile, out_profile);
+#endif
+  convert_to_rgb();
+  
+  im.bps = 16;
+  im.spp = 3;
+  im.New(width, height);
+  
+  // the non-linear gamma by default
+  uint16_t lut [0x10000];
+  const double gamma = 1.8;
+  const double one_over_gamma = 1. / gamma;
+  for (int i = 0; i < 0x10000; ++i)
+    lut [i] = pow( (double) i / 0xFFFF, one_over_gamma) * 0xFFFF;
+  
+  uint16_t* ptr = (uint16_t*) im.data;
+  for (int row = 0; row < height; ++row)
+    for (int col = 0; col < width; ++col)
+      for (int c = 0; c < colors; ++c)
+	*ptr++ = lut[ image[row*width+col][c] ];
+    
+  return true;
+}
+
+bool DCRAWCodec::writeImage (std::ostream* stream, Image& image,
+			     int quality, const std::string& compress)
+{
+  return false;
+}
+
+DCRAWCodec dcraw_loader;
---
+++ tags/0.2.4/lib/rotate.cc
@@ -0,0 +1,310 @@
+
+/*
+ * Copyright (C) 2006 René Rebe
+ *           (C) 2006 Archivista GmbH, CH-8042 Zuerich
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ */
+
+#include <math.h>
+
+#include <iostream>
+#include <iomanip>
+
+#include "ArgumentList.hh"
+
+#include "tiff.hh"
+#include "Image.hh"
+
+using namespace Utility;
+
+void flipX (Image& image)
+{
+  int bytes = (image.w*image.spp*image.bps + 7) / 8;
+  
+  switch (image.spp * image.bps)
+    {
+    case 1:
+      {
+	// create a reversed bit table for fast lookup
+	int reversed_bits[256];
+	for (int i = 0; i < 256; ++i) {
+	  char rev = 0, v = i;
+	  for (int j = 0; j < 8; ++j) {
+	    rev = rev << 1 | v & 1;
+	    v >>= 1;
+	  }
+	  reversed_bits[i] = rev;
+	}
+	
+	for (int y = 0; y < image.h; ++y)
+	  {
+	    unsigned char* row = &image.data [y*bytes];
+	    for (int x = 0; x < bytes/2; ++x) {
+	      unsigned char v = row [x];
+	      row[x] = reversed_bits [row[bytes - 1 - x]];
+	      row[bytes - 1 - x] = reversed_bits[v];
+	    }
+	  }
+      }
+      break;
+    case 8:
+      {
+	for (int y = 0; y < image.h; ++y)
+	  {
+	    unsigned char* row = &image.data [y*bytes];
+	    for (int x = 0; x < bytes/2; ++x) {
+	      unsigned char v = row [x];
+	      row[x] = row[bytes - 1 - x];
+	      row[bytes - 1 - x] = v;
+	    }
+	  }
+      }
+      break;
+    case 24:
+      {
+	for (int y = 0; y < image.h; ++y) {
+	  rgb* rgb_row = (rgb*) &image.data[y*bytes]; 
+	  for (int x = 0; x < image.w/2; ++x) {
+	    rgb v = rgb_row [x];
+	    rgb_row[x] = rgb_row[image.w - 1 - x];
+	    rgb_row[image.w - 1 - x] = v;
+	  }
+	}
+      }
+      break;
+    default:
+      std::cerr << "flipX: unsupported depth." << std::endl;
+      return;
+    }
+}
+
+void flipY (Image& image)
+{
+  // TODO: 16bps
+  int bytes = (image.w*image.spp*image.bps + 7) / 8;
+  for (int y = 0; y < image.h / 2; ++y)
+    {
+      int y2 = image.h - y - 1;
+
+      unsigned char* row1 = &image.data[y*bytes];
+      unsigned char* row2 = &image.data[y2*bytes];
+
+      for (int x = 0; x < bytes; ++x)
+	{
+	  unsigned char v = *row1;
+	  *row1++ = *row2;
+	  *row2++ = v;
+	}
+    }
+}
+
+void shear (Image& image, double xangle, double yangle)
+{
+  if (xangle != 0.0)
+    {
+    }
+  if (yangle != 0.0)
+    {
+    }
+}
+
+void rot90 (Image& image, int angle)
+{
+  bool cw = false; // clock-wise
+  if (angle == 90)
+    cw = true; // else 270 or -90 or whatever and thus counter cw
+    
+  int rot_bytes = (image.h*image.spp*image.bps + 7) / 8;
+  
+  unsigned char* data = image.data;
+  unsigned char* rot_data = (unsigned char*) malloc (rot_bytes * image.w);
+
+  switch (image.spp * image.bps)
+    {
+    case 1:
+      for (int y = 0; y < image.h; ++y) {
+	unsigned char* new_row;
+	if (cw)
+	  new_row = &rot_data [ (image.h - 1 - y) / 8 ];
+	else
+	  new_row = &rot_data [ (image.w - 1) * rot_bytes + y / 8 ];
+	for (int x = 0; x < image.w;) {
+	  // spread the bits thru the various row slots
+	  unsigned char bits = *data++;
+	  int i = 0;
+	  for (; i < 8 && x < image.w; ++i) {
+	    if (cw) {
+	      *new_row = *new_row >> 1 | (bits & 0x80);
+	      new_row += rot_bytes;
+	    }
+	    else {
+	      *new_row = *new_row << 1 | (bits & 0x80) >> 7;
+	      new_row -= rot_bytes;
+	    }
+	    bits <<= 1;
+	    ++x;
+	  }
+	  // finally shift the last line if necessary
+	  if (i < 8) {
+	    if (cw) {
+	      new_row -= rot_bytes;
+	      *new_row = *new_row >> (8 - i);
+	    }
+	    else {
+	      new_row += rot_bytes;
+	      *new_row = *new_row << (8 - i);
+	      
+	    }
+	    bits <<= 1;
+	    ++x;
+	  }
+	}
+      }
+      break;
+      
+    case 8:
+      for (int y = 0; y < image.h; ++y) {
+	unsigned char* new_row;
+	if (cw)
+	  new_row = &rot_data [ image.h - 1 - y ];
+	else
+	  new_row = &rot_data [ (image.w - 1) * rot_bytes + y ];
+	for (int x = 0; x < image.w; ++x) {
+	  *new_row = *data++;
+	  if (cw)
+	    new_row += rot_bytes;
+	  else
+	    new_row -= rot_bytes;
+	}
+      }
+      break;
+      
+    case 24:
+      {
+	rgb* rgb_data = (rgb*) image.data; 
+	for (int y = 0; y < image.h; ++y) {
+	  rgb* new_row;
+	  if (cw)
+	    new_row = (rgb*)
+	      &rot_data [ (image.h - 1 - y) * image.spp ];
+	  else
+	    new_row = (rgb*)
+	      &rot_data [ (image.w - 1) * rot_bytes + (y * image.spp) ];
+	  for (int x = 0; x < image.w; ++x) {
+	    *new_row = *rgb_data++;
+	    if (cw)
+	      new_row += image.h;
+	    else
+	      new_row -= image.h;
+	  }
+	}
+      }
+      break;
+      
+    default:
+      std::cerr << "rot90: unsupported depth. spp: " << image.spp << ", bpp:" << image.bps << std::endl;
+      free (rot_data);
+      return;
+    }
+  
+  // set the new data
+  free (image.data);
+  image.data = rot_data;
+  
+  // we are done, tweak the w/h
+  int x = image.w;
+  image.w = image.h;
+  image.h = x;
+  // resolution, likewise
+  x = image.xres;
+  image.xres = image.yres;
+  image.yres = x;
+}
+
+
+void rotate (Image& image, double angle, Image::iterator background)
+{
+  angle = fmod (angle, 360);
+  if (angle < 0)
+    angle += 360;
+  
+  if (angle == 0.0)
+    return;
+  
+  if (angle == 180.0) {
+    flipX (image);
+    flipY (image);
+    return;
+  }
+
+  if (angle == 90.0) {
+    rot90 (image, 90);
+    return;
+  }
+ 
+  if (angle == 270.0) {
+    rot90 (image, 270);
+    return;
+  }
+
+  // trivial code just for testing, to be optimized
+  
+  angle = angle / 180 * M_PI;
+  
+  const int xcent = image.w/2;
+  const int ycent = image.h/2;
+  
+  Image orig_image = image;
+  
+  image.data = (uint8_t*) malloc (image.Stride()*image.h);
+  Image::iterator it = image.begin();
+  Image::iterator orig_it = orig_image.begin();
+  
+  double cached_sin = sin (angle);;
+  double cached_cos = cos (angle);
+
+  for(int y = 0; y < image.h; ++y)
+      for(int x = 0; x < image.w; ++x)
+	{
+	  double ox =   (x-xcent) * cached_cos + (y-ycent) * cached_sin;
+	  double oy = - (x-xcent) * cached_sin + (y-ycent) * cached_cos;
+	  
+	  ox += xcent;
+	  oy += ycent;
+	  
+	  if (ox >= 0 && oy >= 0 &&
+	      ox < image.w && oy < image.h) {
+	    
+	    int oxx = (int)floor(ox);
+	    int oyy = (int)floor(oy);
+	    
+	    int oxx2 = std::min (oxx+1, image.w);
+	    int oyy2 = std::min (oyy+1, image.h);
+	    
+	    int xdist = (int) ((ox - oxx) * 256);
+	    int ydist = (int) ((oy - oyy) * 256);
+	    
+	    it.set ( (
+		      *orig_it.at (oxx,  oyy ) * (256-xdist) * (256-ydist) +
+		      *orig_it.at (oxx2, oyy ) * xdist       * (256-ydist) +
+		      *orig_it.at (oxx,  oyy2) * (256-xdist) * ydist +
+		      *orig_it.at (oxx2, oyy2) * xdist       * ydist
+		      ) /
+		     (256 * 256) );
+	  }
+	  else
+	    it.set (background);
+	  
+	  ++it;
+	}
+}
