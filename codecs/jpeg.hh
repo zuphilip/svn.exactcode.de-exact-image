d3228995e55b607e00163e3a969177527bcec6be
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-01-09 16:32:42 +0000

    * rehacked jpeg lossless encoding

--- codecs/jpeg.hh
+++ codecs/jpeg.hh
@@ -1,12 +1,21 @@
 #include "Codecs.hh"
 
+extern "C" {
+#include <jpeglib.h>
+#include <jerror.h>
+}
+
 class JPEGCodec : public ImageCodec {
 public:
   
-  JPEGCodec () {
+  JPEGCodec ()
+    : srcinfo (0), src_coef_arrays (0)
+  {
     registerCodec ("jpeg", this);
     registerCodec ("jpg", this);
   };
+  
+  ~JPEGCodec ();
   
   virtual std::string getID () { return "JPEG"; };
   
@@ -14,10 +23,20 @@ public:
   virtual bool writeImage (std::ostream* stream, Image& image,
 			   int quality, const std::string& compress);
   
+  // on-demand decoding
+  virtual /*bool*/ void decodeNow (Image* image);
+  
   // optional optimizing and/or lossless implementations
   virtual bool flipX (Image& image);
   virtual bool flipY (Image& image);
   virtual bool rotate (Image& image, double angle);
   
   virtual bool scale (Image& image, double xscale, double yscale);
+  
+private:
+  // for on-demand loading
+  jpeg_decompress_struct* srcinfo;
+  // for encoding reuse
+  jvirt_barray_ptr* src_coef_arrays;
+
 };
