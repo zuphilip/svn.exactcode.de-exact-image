b71303d71e3f78dc856648afc4fcf0d4878db1ac
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-02-01 17:58:54 +0000

    * tagged 0.3.3

---
+++ tags/0.3.3/codecs/jpeg.hh
@@ -0,0 +1,48 @@
+extern "C" {
+#include <jpeglib.h>
+#include <jerror.h>
+#include "transupp.h"
+}
+
+#include <sstream>
+
+#include "Codecs.hh"
+
+class JPEGCodec : public ImageCodec {
+public:
+  
+  JPEGCodec () {
+    registerCodec ("jpeg", this);
+    registerCodec ("jpg", this);
+  };
+  
+  // freestanding
+  JPEGCodec (Image* _image);
+  
+  virtual std::string getID () { return "JPEG"; };
+  
+  virtual bool readImage (std::istream* stream, Image& image);
+  virtual bool writeImage (std::ostream* stream, Image& image,
+			   int quality, const std::string& compress);
+  
+  // on-demand decoding
+  virtual /*bool*/ void decodeNow (Image* image);
+  
+  // optional optimizing and/or lossless implementations
+  virtual bool flipX (Image& image);
+  virtual bool flipY (Image& image);
+  virtual bool rotate (Image& image, double angle);
+  virtual bool toGray (Image& image);
+  virtual bool scale (Image& image, double xscale, double yscale);
+  
+private:
+
+  void decodeNow (Image* image, int factor);
+  
+  // internals and helper
+  bool readMeta (std::istream* stream, Image& image);
+  bool doTransform (JXFORM_CODE code, Image& image,
+		    std::ostream* stream = 0, bool to_gray = false);
+  
+  std::stringstream private_copy;
+};
