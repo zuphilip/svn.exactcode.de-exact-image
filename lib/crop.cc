71d1034230987ef15928dbe8c0ee44a7bae5bcb2
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-05-18 10:14:59 +0000

    * implemented generic offset crop, optized thru memmove

--- lib/crop.cc
+++ lib/crop.cc
@@ -2,15 +2,12 @@
 #include <algorithm>
 
 #include "Image.hh"
+#include "Colorspace.hh"
 
 #include "crop.hh"
 
 void crop (Image& image, unsigned int x, unsigned int y, unsigned int w, unsigned int h)
 {
-  // something to do?
-  if (x == 0 && y == 0 && w == image.w && h == image.h)
-    return;
-  
   // limit to valid boundaries
   x = std::min (x, (unsigned)image.w-1);
   y = std::min (x, (unsigned)image.h-1);
@@ -18,20 +15,56 @@ void crop (Image& image, unsigned int x, unsigned int y, unsigned int w, unsigne
   w = std::min (x+w, (unsigned)image.w-x);
   h = std::min (y+h, (unsigned)image.h-y);
   
+  // something to do?
+  if (x == 0 && y == 0 && w == image.w && h == image.h)
+    return;
+  
   // TODO: Call thru codec, e.g. optimized for JPEG
   /*
   std::cerr << "after limiting: " << x << " " << y
 	    << " " << w << " " << h << std::endl;
   */
-  
-  // TODO: crop bottom, left and right
-  
-  if (x != 0 || y != 0 || w != image.w) {
-    std::cerr << "crop: offset crop not yet implemented!" << std::endl;
+
+  // truncate the height, this is optimized for the "just height" case
+  // (of e.g. fastAutoCrop)
+  if (x == 0 || y == 0 || w == image.w) {
+    image.setRawData (); // invalidate
+    image.h = h;
     return;
   }
   
-  // invalidate data
-  image.setRawData ();
-  image.h = h;
+  // bit shifting is too expensive, crop at least byte-wide
+  int orig_bps = image.bps;
+  if (orig_bps < 8)
+    colorspace_grayX_to_gray8 (image);
+  
+  int stride = image.Stride();
+  int cut_stride = stride * w / image.w;
+  
+  uint8_t* dst = image.getRawData ();
+  uint8_t* src = dst + stride * y;
+  
+  for (int i = 0; i < h; ++i) {
+    memmove (dst, src, cut_stride);
+    dst += cut_stride;
+    src += stride;
+  }
+  
+image.setRawData (); // invalidate
+ image.w = w;
+ image.h = h;
+ 
+ switch (orig_bps) {
+ case 1:
+   colorspace_gray8_to_gray1 (image);
+   break;
+ case 2:
+   colorspace_gray8_to_gray2 (image);
+   break;
+ case 4:
+   colorspace_gray8_to_gray4 (image);
+   break;
+ default:
+   ;
+ }
 }
