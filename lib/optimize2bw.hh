0ffe4d50f02a76ac6190ca3d5975f78e16779d7a
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-07-04 08:07:39 +0000

    * added an work in progress differential image thresholding

--- lib/optimize2bw.hh
+++ lib/optimize2bw.hh
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2005 René Rebe
- *           (C) 2005 Archivista GmbH, CH-8042 Zuerich
- * 
+ * Copyright (C) 2005 - 2007 René Rebe
+ *           (C) 2005 - 2007 Archivista GmbH, CH-8042 Zuerich
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 2. A copy of the GNU General
@@ -24,4 +24,16 @@
 
 void optimize2bw (Image& image, int low = 0, int high = 0, int threshold = 0,
 		  int sloppy_threshold = 0,
-		  int radius = 3, double standard_deviation = 2.1);
+		  int radius = 3,
+		  double standard_deviation = 2.1);
+
+// (just - for now -) perform move a differential operator over the
+// image to get the contour, we'll expans this later to a real
+// alternative, and faster bw  optimization - for now mostly intended
+// to speedup the contour matching - parameters are unused, just kept
+// as drop in replacement for the optimize2bw
+void differential_optimize2bw (Image& image,
+			       int low = 0, int high = 0, int threshold = 0,
+			       int sloppy_threshold = 0,
+			       int radius = 3,
+			       double standard_deviation = 2.1);
