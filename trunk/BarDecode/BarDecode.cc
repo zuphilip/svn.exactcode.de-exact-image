e25495a288243920509511e7c30edf299083ee7c
lars <lars@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-10-24 12:39:25 +0000

    * Cleanup code in BarDecode.cc.

--- trunk/BarDecode/BarDecode.cc
+++ trunk/BarDecode/BarDecode.cc
@@ -6,18 +6,13 @@
 
 #include "ArgumentList.hh"
 #include "Codecs.hh"
-#include "Colorspace.hh"
-#include "Matrix.hh"
-#include "FG-Matrix.hh"
-#include "optimize2bw.hh"
 
 #include "BarDecodeModulizer.hh"
 #include "BarDecodeScanner.hh"
 
-
 using namespace Utility;
 
-
+#ifdef BARDECODE_DEBUG
 // not very efficient, yet effective
 void PutPixel(Image& img, int x, int y, uint16_t R, uint16_t G,  uint16_t B)
 {
@@ -55,6 +50,7 @@ void Draw(Image& img, BarDecode::threshold_t threshold, unsigned int r, unsigned
         } 
     }
 }
+#endif
 
 
 int main (int argc, char* argv[])
@@ -66,35 +62,24 @@ int main (int argc, char* argv[])
 			   "display this help text and exit");
   Argument<std::string> arg_input ("i", "input", "input file", 1, 1);
 
-  Argument<std::string> arg_output ("o", "output", "output file", 0,1);
-
-  // optimize2bw options
-
-  Argument<int> arg_low ("l", "low",
-			 "low normalization value", 0, 0, 1);
-  Argument<int> arg_high ("h", "high",
-			  "high normalization value", 0, 0, 1);
-  
   Argument<int> arg_threshold ("t", "threshold",
 			       "bi-level threshold value", 0, 0, 1);
 
-  Argument<int> arg_radius ("r", "radius",
-			    "\"unsharp mask\" radius", 0, 0, 1);
-
   arglist.Add (&arg_help);
   arglist.Add (&arg_input);
-  arglist.Add (&arg_output);
-  arglist.Add (&arg_low);
-  arglist.Add (&arg_high);
   arglist.Add (&arg_threshold);
-  arglist.Add (&arg_radius);
 
+#ifdef BARDECODE_DEBUG
+  Argument<std::string> arg_output ("o", "output", "output file", 0,1);
+  arglist.Add (&arg_output);
+#endif
 
   // parse the specified argument list - and maybe output the Usage
   if (!arglist.Read (argc, argv) || arg_help.Get() == true)
     {
-      std::cerr << "Based on Color / Gray image to Bi-level optimizer"
-                <<  " - Copyright 2005, 2006 by René Rebe" << std::endl
+      std::cerr << "barcode recognition module of the exact-image library"
+                <<  " - Copyright 2007 - 2008 by René Rebe" << std::endl
+                <<  " - Copyright 2007 - 2008 by Lars Kuhtz" << std::endl
                 << "Usage:" << std::endl;
       
       arglist.Usage (std::cerr);
@@ -107,36 +92,13 @@ int main (int argc, char* argv[])
     return 1;
   }
 
-  int low = 0;
-  int high = 170; // seems a god choice
-  int sloppy_threshold = 0;
-  int radius = 3;
-  double sd = 2.1;
-  
-  if (arg_low.Get() != 0) {
-    low = arg_low.Get();
-    std::cerr << "Low value overwritten: " << low << std::endl;
-  }
-  
-  if (arg_high.Get() != 0) {
-    high = arg_high.Get();
-    std::cerr << "High value overwritten: " << high << std::endl;
-  }
-  
-  if (arg_radius.Get() != 0) {
-    radius = arg_radius.Get();
-    std::cerr << "Radius: " << radius << std::endl;
-  }
-  
   // convert to 1-bit (threshold)
   int threshold = 0;
-  
   if (arg_threshold.Get() != 0) {
     threshold = arg_threshold.Get();
+  } else {
+    threshold = 130;
   }
-
-  if (arg_threshold.Get() == 0)
-    threshold = 170;
 
   std::set<std::string> codes;
   BarDecode::BarCodeIterator it(&image,threshold);
@@ -151,6 +113,7 @@ int main (int argc, char* argv[])
       std::cout << *it << std::endl;
   }
 
+#ifdef BARDECODE_DEBUG
   if (arg_output.Get() != "") {
       Image o_image = image;
       Draw(o_image,threshold,0,0,255);
@@ -160,6 +123,7 @@ int main (int argc, char* argv[])
           return 1;
       }
   }
+#endif
  
   return codes.empty();
 }
