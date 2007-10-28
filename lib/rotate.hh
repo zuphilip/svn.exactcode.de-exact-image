0b0749dbaf69a33b8ebaa6c84b68e60efff56aea
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2007-10-28 08:54:03 +0000

    * fixed copy_crop_rotate to also allow negative start coordinates

--- lib/rotate.hh
+++ lib/rotate.hh
@@ -6,6 +6,6 @@ void flipY (Image& image);
 void shear (Image& image, double xangle, double yangle);
 void rotate (Image& image, double angle, Image::iterator background);
 
-Image* copy_crop_rotate (Image& image, unsigned int x_start, unsigned int y_start,
+Image* copy_crop_rotate (Image& image, int x_start, int y_start,
 			 unsigned int w, unsigned int h,
 			 double angle);
