09b5e49c22dfb269be4b549db2f69d29512cdfe4
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-01-15 16:43:15 +0000

    * switched optimize2bw to new, sym. decomposeable matrix code, 	  faster and more accurate (except border pixels) 	* removed obsolete private hacky convolution 	* improved / cleaned commented debug timer to have more style

--- lib/optimize2bw.cc
+++ lib/optimize2bw.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 - 2007 René Rebe
+ * Copyright (C) 2005 - 2008 René Rebe, ExactCODE GmbH
  *           (C) 2005 - 2007 Archivista GmbH, CH-8042 Zuerich
  * 
  * This program is free software; you can redistribute it and/or modify
@@ -43,112 +43,7 @@
 
 #include "optimize2bw.hh"
 
-//#include "Timer.hh"
 //#include "Timer.cc"
-
-/* private copy of the convolution_matrix, checking for edges at the
-   center of the destination pixel in order to skip the expensive
-   matrix convolution and thus improve performance */
-
-/*
-19:41 <valentin> btw, your original code might have an off-by-2 bug
-19:41 <valentin> but i am not sure because it is too complicated
-19:42 <valentin> you transform the strip xr <= x <= image.w-xr
-19:42 <valentin> but if xw=7 then xr=3
-*/
-
-static void private_convolution_matrix (Image& image, matrix_type* matrix, int xw, int yw,
-					matrix_type divisor)
-{
-  uint8_t* data = image.getRawData();
-  uint8_t* new_data = (uint8_t*) malloc (image.w * image.h);
-  
-  const int xr = xw / 2;
-  const int yr = yw / 2;
-
-  const int _y1 = std::min (image.h, yr);
-  const int _y2 = std::max (image.h-yr, yr);
-
-  uint8_t* src_ptr = data;
-  uint8_t* dst_ptr = new_data;
-
-  const int kernel_offset = yr * image.w + xr;
-
-  // top-border
-  for (int i = 0; i < _y1 * image.w; ++i)
-    *dst_ptr++ = *src_ptr++;
-  
-  divisor = 1 / divisor; // to multiple in the loop
-  
-  for (int y = _y1; y < _y2; ++y)
-  {
-    src_ptr = &data[y * image.w];
-    dst_ptr = &new_data[y * image.w];
-
-    // left-side border
-    for (int x = 0; x < xr; ++x)
-        *dst_ptr++ = *src_ptr++;
-
-    // convolution area
-    for (int x = xr; x < image.w-xr; ++x)
-      {
-	  uint8_t* data_row = src_ptr++ - kernel_offset;
-	  
-	  // if no local edge, copy pixel and exit early
-	  {
-	    uint8_t* it = data_row + kernel_offset;
-	    uint8_t ref = *it;
-	    
-	    int diff = std::abs ( (int)it[-1-image.w] - ref ) +
-	      std::abs ( (int)it[+1-image.w] - ref ) +
-	      std::abs ( (int)it[-1+image.w] - ref ) +
-	      std::abs ( (int)it[+1+image.w] - ref );
-	    
-	    if (diff < 4*4) {
-	      *dst_ptr++ = ref;
-	      continue;
-	    }
-	  }
-	  
-	  matrix_type sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
-
-	  matrix_type* matrix_row = matrix;
-	  // in former times this was more readable and got overoptimized
-	  // for speed ,-)
-	  for (int y2 = 0; y2 < yw; ++y2, data_row += image.w - xw) {
-	    int x2 = xw;
-	    while (x2 >= 4) {
-	      sum1 += matrix_row[0] * data_row[0];
-	      sum2 += matrix_row[1] * data_row[1];
-	      sum3 += matrix_row[2] * data_row[2];
-	      sum4 += matrix_row[3] * data_row[3];
-	      data_row += 4;
-	      matrix_row += 4;
-	      x2 -= 4;
-	    }
-	    while (x2 > 0) {
- 	      sum1 += *matrix_row++ * *data_row++;
-	      --x2;
-	    }
-	  }
-	  
-	  matrix_type sum = (sum1+sum2+sum3+sum4) * divisor;
-	  uint8_t z = (uint8_t) (sum > 255 ? 255 : sum < 0 ? 0 : sum);
-	  *dst_ptr++ = z;
-      }
-    // right-side border
-    for (int x = image.w-xr; x < image.w; ++x)
-        *dst_ptr++ = *src_ptr++;
-  }
-
-  // bottom-border
-  for (int i = 0; i < image.w * (image.h-_y2); ++i)
-    *dst_ptr++ = *src_ptr++;
-
-  image.setRawData (new_data);
-}
-
-
 
 void optimize2bw (Image& image, int low, int high, int threshold,
 		  int sloppy_threshold,
@@ -257,60 +152,11 @@ void optimize2bw (Image& image, int low, int high, int threshold,
     image.setRawData();
   }
 
-  //std::cerr << "convolution" << std::endl;
-  //Utility::Timer timer;
-  //timer.Reset();
-
-#define OLD_CODE
-#ifdef OLD_CODE  
   // Convolution Matrix (unsharp mask a-like)
   {
     // compute kernel (convolution matrix to move over the iamge)
+    // Utility::AutoTimer<Utility::Timer> timer ("convolution");
     
-    const int width = radius * 2 + 1;
-    matrix_type divisor = 0;
-    float sd = standard_deviation;
-    
-    matrix_type* matrix = new matrix_type [width * width];
-    
-    if (debug)
-      std::cerr << std::fixed << std::setprecision(3);
-    for (int y = -radius; y <= radius; ++y) {
-      for (int x = -radius; x <= radius; ++x) {
-	matrix_type v = (matrix_type) (exp (-((float)x*x + (float)y*y) / (2. * sd * sd)) * 5);
-	divisor += v;
-	
-	matrix[x + radius + (y+radius)*width] = v;
-      }
-    }
-    
-    // sub from image *2 and print
-    for (int y = -radius; y <= radius; ++y) {
-      for (int x = -radius; x <= radius; ++x) {
-	matrix_type* m = &matrix[x + radius + (y+radius)*width];
-	
-	
-	*m *= -1;
-	if (x == 0 && y == 0)
-	  *m += 2*divisor;
-		
-
-	if (debug)
-	  std::cerr << *m << " ";
-      }
-      if (debug)
-	std::cerr << std::endl;
-    }
-    
-    private_convolution_matrix (image, matrix, width, width, divisor);
-    delete (matrix);
-  }
-#else
-
-  // Convolution Matrix (unsharp mask a-like)
-  {
-    // compute kernel (convolution matrix to move over the iamge)
-
     matrix_type divisor = 0;
     float sd = standard_deviation;
     
@@ -333,13 +179,7 @@ void optimize2bw (Image& image, int low, int high, int threshold,
     }
     
     decomposable_sym_convolution_matrix (image, matrix, matrix_2, radius, radius, 2.0);
-
   }
-
-#endif
-
-  //  std::cerr << "done " << timer.Delta() << std::endl;
-
 }
 
 void differential_optimize2bw (Image& image,
