982de60d7ba8dfba1206e85148b21e3e4cb46a99
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-01-20 16:26:14 +0000

    * merged r899 from trunk: "fixed" copy_crop_rotate to allow passing a 	  dedicated background color to fill "out of bounds" pixels

--- api/api.cc
+++ api/api.cc
@@ -39,12 +39,6 @@ Image* copyImage (Image* other)
   return image;
 }
 
-Image* copyImageCropRotate (Image* image, unsigned int x, unsigned int y,
-			   unsigned int w, unsigned int h, double angle)
-{
-  return copy_crop_rotate (*image, x, y, w, h, angle);
-}
-
 bool decodeImage (Image* image, char* data, int n)
 {
   const std::string str (data, n); 
@@ -152,6 +146,12 @@ bool imageConvertColorspace (Image* image, const char* target_colorspace)
 void imageRotate (Image* image, double angle)
 {
   rotate (*image, angle, background_color);
+}
+
+Image* copyImageCropRotate (Image* image, unsigned int x, unsigned int y,
+			   unsigned int w, unsigned int h, double angle)
+{
+  return copy_crop_rotate (*image, x, y, w, h, angle, background_color);
 }
 
 void imageFlipX (Image* image)
