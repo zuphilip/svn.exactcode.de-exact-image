e11744125c55d3a60daa90fba2e13c4514cae149
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-02-09 19:29:17 +0000

    * improved external API to export a second encodeImage returning 	  a C++ string 	* added glue code to make the in-memory functions usable from 	  languages where SWIG does (not yet) implement cstring support 	  (e.g. Lua)

--- api/api-swig.hh
+++ api/api-swig.hh
@@ -1,6 +1,23 @@
+/*
+ * The ExactImage stable external API for use with SWIG.
+ * Copyright (C) 2006 - 2008 René Rebe, ExactCODE GmbH
+ * Copyright (C) 2006 René Rebe, Archivista
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * 
+ */
 
 %module ExactImage
 %include "cstring.i"
+%include "std_string.i"
 
 # manually include it, otherwise SWIG will not source it
 %include "config.h"
