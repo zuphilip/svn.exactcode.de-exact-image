dfbe5a56664a2e592ced9b953692dde174004a1b
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-04-08 08:38:04 +0000

    * removed external, binary-only blob wrapping bardecode.?? 	* moved wrapper into the external API module, the only place 	  where we have to continue to support it for a while

--- trunk/lib/bardecode.hh
+++
@@ -1,7 +0,0 @@
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> decodeBarcodes (Image& image, const std::string& codes,
-					 unsigned int min_length,
-                                         unsigned int max_length, int multiple);
