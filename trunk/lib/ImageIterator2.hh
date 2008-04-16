8e2f6d134dc5e98ba40515fc8f15c76262d5c978
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-04-16 09:03:11 +0000

    * added rgb16 and gray16 support and a saturate method to the accumulator 	  of the ImageIterator2

--- trunk/lib/ImageIterator2.hh
+++ trunk/lib/ImageIterator2.hh
@@ -28,8 +28,16 @@ public:
   class accu
   {
   public:
-    unsigned int v1, v2, v3;
-      
+    signed int v1, v2, v3;
+    
+    accu () { v1 = v2 = v3 = 0; }
+    
+    void saturate () {
+      v1 = std::min (std::max (v1, 0), 0xff);
+      v2 = std::min (std::max (v2, 0), 0xff);
+      v3 = std::min (std::max (v3, 0), 0xff);
+    }
+    
     accu& operator*= (int f) {
       v1 *= f;
       v2 *= f;
@@ -55,15 +63,14 @@ public:
       v3 += other.v3;
       return *this;
     }
-
-
+    
     accu& operator= (const Image::iterator& background)
     {
-      double r, g, b;
+      double r = 0, g = 0, b = 0;
       background.getRGB(r, g, b);
-      v1 = r * 255;
-      v2 = g * 255;
-      v3 = b * 255;
+      v1 = r * 0xff;
+      v2 = g * 0xff;
+      v3 = b * 0xff;
       return *this;
     }
   };
@@ -100,6 +107,97 @@ public:
     
 };
 
+class rgb16_iterator
+{
+public:
+  uint16_t* ptr;
+  uint16_t* ptr_begin;
+  const Image& image;
+  const int stride;
+
+    
+  class accu
+  {
+  public:
+    signed int v1, v2, v3;
+    
+    accu () { v1 = v2 = v3 = 0; }
+    
+    void saturate () {
+      v1 = std::min (std::max (v1, 0), 0xffff);
+      v2 = std::min (std::max (v2, 0), 0xffff);
+      v3 = std::min (std::max (v3, 0), 0xffff);
+    }
+    
+    accu& operator*= (int f) {
+      v1 *= f;
+      v2 *= f;
+      v3 *= f;
+      return *this;
+    }
+
+    accu operator* (int f) {
+      accu a = *this;
+      return a *= f;
+    }
+      
+    accu& operator/= (int f) {
+      v1 /= f;
+      v2 /= f;
+      v3 /= f;
+      return *this;
+    }
+      
+    accu& operator+= (const accu& other) {
+      v1 += other.v1;
+      v2 += other.v2;
+      v3 += other.v3;
+      return *this;
+    }
+    
+    accu& operator= (const Image::iterator& background)
+    {
+      double r = 0, g = 0, b = 0;
+      background.getRGB(r, g, b);
+      v1 = r * 0xffff;
+      v2 = g * 0xffff;
+      v3 = b * 0xffff;
+      return *this;
+    }
+  };
+  
+  rgb16_iterator (Image& _image)
+    : ptr_begin((uint16_t*)_image.getRawData()), image (_image),
+      stride(_image.stride()) {
+    ptr = ptr_begin;
+  }
+  
+  rgb16_iterator& at (int x, int y) {
+    ptr = ptr_begin + y * stride / 2 + x * 3;
+    return *this;
+  }
+    
+  rgb16_iterator& operator++ () {
+    ptr += 3;
+    return *this;
+  }
+    
+  accu operator* () {
+    accu a;
+    a.v1 = ptr[0];
+    a.v2 = ptr[1];
+    a.v3 = ptr[2];
+    return a;
+  }
+    
+  rgb16_iterator& set (const accu& a) {
+    ptr[0] = a.v1;
+    ptr[1] = a.v2;
+    ptr[2] = a.v3;
+    return *this;
+  }    
+};
+
 class gray_iterator
 {
 public:
@@ -111,8 +209,14 @@ public:
   class accu
   {
   public:
-    unsigned int v1;
+    signed int v1;
+
+    accu () { v1 = 0; }
       
+    void saturate () {
+      v1 = std::min (std::max (v1, 0), 0xff);
+    }
+
     accu& operator*= (int f) {
       v1 *= f;
       return *this;
@@ -169,6 +273,82 @@ public:
     
 };
 
+class gray16_iterator
+{
+public:
+  uint16_t* ptr;
+  uint16_t* ptr_begin;
+  const Image& image;
+  const int stride;
+    
+  class accu
+  {
+  public:
+    signed int v1;
+
+    accu () { v1 = 0; }
+      
+    void saturate () {
+      v1 = std::min (std::max (v1, 0), 0xffff);
+    }
+
+    accu& operator*= (int f) {
+      v1 *= f;
+      return *this;
+    }
+
+    accu operator* (int f) {
+      accu a = *this;
+      return a *= f;
+    }
+      
+    accu& operator/= (int f) {
+      v1 /= f;
+      return *this;
+    }
+      
+    accu& operator+= (const accu& other) {
+      v1 += other.v1;
+      return *this;
+    }
+      
+    accu& operator= (const Image::iterator& background)
+    {
+      v1 = background.getL();
+      return *this;
+    }
+      
+  };
+    
+  gray16_iterator (Image& _image)
+    : ptr_begin((uint16_t*)_image.getRawData()), image (_image),
+      stride(_image.stride()) {
+    ptr = ptr_begin;
+  }
+    
+  gray16_iterator& at (int x, int y) {
+    ptr = ptr_begin + y * stride/2 + x;
+    return *this;
+  }
+    
+  gray16_iterator& operator++ () {
+    ptr += 1;
+    return *this;
+  }
+    
+  accu operator* () {
+    accu a;
+    a.v1 = ptr[0];
+    return a;
+  }
+    
+  gray16_iterator& set (const accu& a) {
+    ptr[0] = a.v1;
+    return *this;
+  }
+    
+};
+
 template <unsigned int bitdepth>
 class bit_iterator
 {
@@ -211,7 +391,7 @@ public:
     
   accu operator* () {
     accu a;
-    a.v1 = ((*ptr >> (bitpos - (bitdepth - 1))) & mask) * 255 / mask;
+    a.v1 = ((*ptr >> (bitpos - (bitdepth - 1))) & mask) * 0xff / mask;
     return a;
   }
     
@@ -227,8 +407,17 @@ template <template <typename T> class ALGO, class T1, class T2, class T3>
 void codegen (T1& a1, T2& a2, T3& a3)
 {
   if (a1.spp == 3) {
-    ALGO <rgb_iterator> a;
-    a (a1, a2, a3);
+    if (a1.bps == 8) {
+      ALGO <rgb_iterator> a;
+      a (a1, a2, a3);
+    } else {
+      ALGO <rgb16_iterator> a;
+      a (a1, a2, a3);
+    }
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
+    a(a1, a2, a3);
   }
   else if (a1.bps == 8) {
     ALGO <gray_iterator> a;
@@ -248,6 +437,74 @@ void codegen (T1& a1, T2& a2, T3& a3)
   }
 }
 
+template <template <typename T> class ALGO, class T1, class T2, class T3, class T4, class T5>
+void codegen (T1& a1, T2& a2, T3& a3, T4& a4, T5& a5)
+{
+  if (a1.spp == 3) {
+    if (a1.bps == 8) {
+      ALGO <rgb_iterator> a;
+      a (a1, a2, a3, a4, a5);
+    } else {
+      ALGO <rgb16_iterator> a;
+      a (a1, a2, a3, a4, a5);
+    }
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
+    a(a1, a2, a3, a4, a5);
+  }
+  else if (a1.bps == 8) {
+    ALGO <gray_iterator> a;
+    a(a1, a2, a3, a4, a5);
+  }
+  else if (a1.bps == 4) {
+    ALGO <bit_iterator<4> > a;
+    a (a1, a2, a3, a4, a5);
+  }
+  else if (a1.bps == 2) {
+    ALGO <bit_iterator<2> > a;
+    a (a1, a2, a3, a4, a5);
+  }
+  else if (a1.bps == 1) {
+    ALGO <bit_iterator<1> > a;
+    a (a1, a2, a3, a4, a5);
+  }
+}
+
+template <template <typename T> class ALGO, class T1, class T2, class T3, class T4, class T5,  class T6>
+void codegen (T1& a1, T2& a2, T3& a3, T4& a4, T5& a5, T6& a6)
+{
+  if (a1.spp == 3) {
+    if (a1.spp == 8) {
+      ALGO <rgb_iterator> a;
+      a (a1, a2, a3, a4, a5, a6);
+    } else {
+      ALGO <rgb16_iterator> a;
+      a (a1, a2, a3, a5, a5, a6);
+    }
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
+    a(a1, a2, a3, a4, a5, a6);
+  }
+  else if (a1.bps == 8) {
+    ALGO <gray_iterator> a;
+    a(a1, a2, a3, a4, a5, a6);
+  }
+  else if (a1.bps == 4) {
+    ALGO <bit_iterator<4> > a;
+    a (a1, a2, a3, a4, a5, a6);
+  }
+  else if (a1.bps == 2) {
+    ALGO <bit_iterator<2> > a;
+    a (a1, a2, a3, a4, a5, a6);
+  }
+  else if (a1.bps == 1) {
+    ALGO <bit_iterator<1> > a;
+    a (a1, a2, a3, a4, a5, a6);
+  }
+}
+
 template <template <typename T> class ALGO,
 	  class T1, class T2, class T3, class T4,
 	  class T5, class T6, class T7>
@@ -255,7 +512,16 @@ void codegen (T1& a1, T2& a2, T3& a3, T4& a4,
 	      T5& a5, T6& a6, T7& a7)	   
 {
   if (a1.spp == 3) {
-    ALGO <rgb_iterator> a;
+    if (a1.bps == 8) {
+      ALGO <rgb_iterator> a;
+      a (a1, a2, a3, a4, a5, a6, a7);
+    } else {
+      ALGO <rgb16_iterator> a;
+      a (a1, a2, a3, a4, a5, a6, a7);
+    }
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
     a (a1, a2, a3, a4, a5, a6, a7);
   }
   else if (a1.bps == 8) {
@@ -285,7 +551,16 @@ T0 codegen_return (T1& a1, T2& a2, T3& a3, T4& a4,
 		   T5& a5, T6& a6, T7& a7)	   
 {
   if (a1.spp == 3) {
-    ALGO <rgb_iterator> a;
+    if (a1.bps == 8) {
+      ALGO <rgb_iterator> a;
+      return a (a1, a2, a3, a4, a5, a6, a7);
+    } else {
+      ALGO <rgb16_iterator> a;
+      return a (a1, a2, a3, a4, a5, a6, a7);
+    }
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
     return a (a1, a2, a3, a4, a5, a6, a7);
   }
   else if (a1.bps == 8) {
