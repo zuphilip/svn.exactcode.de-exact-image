e9380b83273379c73e993f7b0a9cb3e090fc6fb7
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-05-21 09:40:49 +0000

    * added copy constructur and copyright note to FG-Matrix

--- trunk/lib/FG-Matrix.cc
+++ trunk/lib/FG-Matrix.cc
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2007 - 2008 Valentin Ziegler, ExactCODE GmbH Germany.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
+
 #include "FG-Matrix.hh"
 
 FGMatrix::FGMatrix(Image& image, unsigned int fg_threshold)
@@ -15,6 +32,11 @@ FGMatrix::FGMatrix(Image& image, unsigned int fg_threshold)
       row=0;
     }
   }
+}
+
+FGMatrix::FGMatrix(const FGMatrix& source)
+  : DataMatrix<bool>(source, 0, 0, source.w, source.h)
+{
 }
 
 FGMatrix::FGMatrix(const FGMatrix& source, unsigned int x, unsigned int y, unsigned int w, unsigned int h)
