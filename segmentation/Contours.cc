ea1891096963182f44bbb712c7f0505581e2916c
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-05-22 13:41:58 +0000

    * fixed some compile warnings, and excessive logging in segmentation examples

--- segmentation/Contours.cc
+++ segmentation/Contours.cc
@@ -1,4 +1,19 @@
-//#include <math.h>
+/*
+ * Copyright (C) 2007 - 2008 Valentin Ziegler, ExactCODE GmbH Germany.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
 
 #include <iostream>
 #include <iomanip>
@@ -142,7 +157,6 @@ int main (int argc, char* argv[])
     case 5: r=255; g=0; b=255; break;
     }
     DrawContour(o_image, *cont.contours[i], r,g,b);
-    std::cout << cont.contours[i]->size() << std::endl;
   }
 
   if (!ImageCodec::Write(arg_output.Get(), o_image)) {
--- segmentation/distance.cc
+++ segmentation/distance.cc
@@ -1,6 +1,5 @@
 /*
- * Image Segmentation
- * Copyright (C) 2007 Valentin Ziegler and René Rebe
+ * Copyright (C) 2007 - 2008 Valentin Ziegler, ExactCODE GmbH Germany.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,7 +10,9 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
  * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
  */
 
 #include <math.h>
@@ -136,10 +137,11 @@ int main (int argc, char* argv[])
   Image::iterator color=o_image.begin();
   for (; i!=end ; ++i) {
     int value=255-(int)dm(row,line);
-    color.setRGB(value < 0 ? 0 : value, value==255 ? 255 : 0, 0);
+    uint16_t r = value < 0 ? 0 : value, g = value==255 ? 255 : 0, b = 0;
+    color.setRGB(r, g, b);
     i.set(color);
    
-    if (++row == image.w) {
+    if (++row == (unsigned int)image.w) {
       line++;
       row=0;
     }
