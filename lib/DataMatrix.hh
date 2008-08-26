6e46f34a514a9ff3aeadd3ca76e06055a320c165
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-08-26 12:35:02 +0000

    * added copyright header to DataMatrix.hh

--- lib/DataMatrix.hh
+++ lib/DataMatrix.hh
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2007 - 2008 Valentin Ziegler, ExactCODE GmbH Germany.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
+
 #ifndef DATA_MATRIX_HH__
 #define DATA_MATRIX_HH__
 
