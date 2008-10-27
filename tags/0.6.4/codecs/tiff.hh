d9973293ef791a1d8e5629c6ed82454cd8d12139
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-10-27 16:01:33 +0000

    * tagged 0.6.4

---
+++ tags/0.6.4/codecs/tiff.hh
@@ -0,0 +1,22 @@
+
+#include "Codecs.hh"
+
+// not TIFFCodec due the TIFFCodec in libtiff itself ...
+class TIFCodec : public ImageCodec {
+public:
+  
+  TIFCodec () {
+    registerCodec ("tiff", this);
+    registerCodec ("tif", this);
+  };
+  
+  virtual std::string getID () { return "TIFF"; };
+  
+  virtual bool readImage (std::istream* stream, Image& image);
+  virtual bool writeImage (std::ostream* stream, Image& image,
+			   int quality, const std::string& compress);
+  
+//private:
+  
+  static bool writeImageImpl (TIFF* out, const Image& image, const std::string& conpress, int page = 0);
+};
---
+++ tags/0.6.4/lib/Image.cc
@@ -0,0 +1,152 @@
+/*
+ * The Plain Old Data encapsulation of pixel, raster data.
+ * Copyright (C) 2005 - 2008 René Rebe
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
+ 
+#include <string.h> // memcpy
+#include <iostream>
+
+#define DEPRECATED
+#include "Image.hh"
+#include "Codecs.hh"
+
+Image::Image ()
+  : data(0), modified(false), codec(0)
+{
+}
+
+Image::Image (Image& other)
+  : data(0), modified(false), codec(0)
+{
+  operator= (other);
+}
+
+Image::~Image () {
+  // release attached codec
+  if (codec)
+    delete (codec); codec = 0;
+      
+  // release POD
+  if (data)
+    free (data); data = 0;
+}
+
+void Image::copyMeta (const Image& other)
+{
+  w = other.w;
+  h = other.h;
+  bps = other.bps;
+  spp = other.spp;
+  xres = other.xres;
+  yres = other.yres;
+}
+
+Image& Image::operator= (const Image& other)
+{
+  copyMeta (other);
+  
+  uint8_t* d = other.getRawData();
+  if (d) {
+    resize (w, h);
+    memcpy (data, d, stride() * h);
+  }
+  else {
+    setRawData();
+  }
+  
+  return *this;
+}
+
+void Image::copyTransferOwnership (Image& other)
+{
+  copyMeta (other);
+
+  uint8_t* d = other.getRawData();
+  other.setRawDataWithoutDelete (0);
+  setRawData (d);
+}
+
+uint8_t* Image::getRawData () const {
+  // ask codec about it
+  if (!data && codec) {
+    Image* image = const_cast<Image*>(this);
+    codec->decodeNow (image);
+    if (data) // if data was added
+      image->modified = false;
+  }
+  return data;
+}
+
+uint8_t* Image::getRawDataEnd () const {
+  // we call getRawData as it might have to query the codec to actually load it
+  return getRawData() + h * stride();
+}
+
+void Image::setRawData () {
+  if (!modified) {
+    // DEBUG
+    // std::cerr << "Image modified" << std::endl;
+    modified = true;
+  }
+}
+
+void Image::setRawData (uint8_t* _data) {
+  if (_data != data && data) {
+    free (data);
+    data = 0;
+  }
+
+  // reuse:
+  setRawDataWithoutDelete (_data);
+}
+
+void Image::setRawDataWithoutDelete (uint8_t* _data) {
+  data = _data;
+  
+  // reuse
+  setRawData ();
+}
+
+void Image::resize (int _w, int _h) {
+  w = _w;
+  h = _h;
+  
+  // reuse:
+  setRawDataWithoutDelete ((uint8_t*) realloc (data, stride() * h));
+}
+
+void Image::setDecoderID (const std::string& id) {
+  decoderID = id;
+}
+
+const std::string& Image::getDecoderID () {
+  return decoderID;
+}
+
+ImageCodec* Image::Image::getCodec() {
+  return codec;
+}
+
+void Image::setCodec (ImageCodec* _codec) {
+  codec = _codec;
+  // at the moment the codec is attached the data recent
+  if (codec) // not 0
+    modified = false;
+}
+
+bool Image::isModified () {
+  return modified;
+}
