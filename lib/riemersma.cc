e3335246640842f645ac81a955eb44ede18dad35
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-11-12 15:50:19 +0000

    * c++ comments in the riemersma code, as well as less explicit casts

--- lib/riemersma.cc
+++ lib/riemersma.cc
@@ -1,4 +1,20 @@
-/* Riemersma dither
+/*
+ * Colorspace conversions..
+ * Copyright (C) 2006 - 2008 René Rebe, ExactCOD GmbH Germany
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ *
  * Based uppon:
  *   C/C++ Users Journal, December 1998: A Balanced Dithering Technique
  *                                       Thiadmer Riemersma
@@ -16,18 +32,18 @@ enum direction_t {
   RIGHT,
 };
 
-/* variables needed for the Riemersma dither algorithm */
+// variables needed for the Riemersma dither algorithm
 static int cur_x = 0, cur_y = 0;
 static int img_width = 0, img_height = 0;
 static int img_bytes = 0;
 static float img_factor;
 static uint8_t* img_ptr;
 
-#define SIZE 16                 /* queue size: number of pixels remembered */
-#define MAX  16                 /* relative weight of youngest pixel in the
-                                 * queue, versus the oldest pixel */
+#define SIZE 16                 // queue size: number of pixels remembered
+#define MAX  16                 // relative weight of youngest pixel in the
+                                // queue, versus the oldest pixel
 
-static int weights[SIZE];       /* weights for the errors of recent pixels */
+static int weights[SIZE];       // weights for the errors of recent pixels
 
 static void init_weights(int a[], int size, int max)
 {
@@ -36,14 +52,14 @@ static void init_weights(int a[], int size, int max)
   int i;
 
   for (i = 0, v = 1.0; i < size; i++) {
-    a[i] = (int)(v + 0.5);  /* store rounded value */
-    v *= m;                 /* next value */
+    a[i] = (int)(v + 0.5);  // store rounded value
+    v *= m;                 // next value
   }
 }
 
 static void dither_pixel(uint8_t *pixel)
 {
- static int error[SIZE]; /* queue with error values of recent pixels */
+  static int error[SIZE]; // queue with error values of recent pixels
  int err = 0L;
  for (int i = 0; i < SIZE; i++)
     err += error[i] * weights[i];
@@ -56,19 +72,19 @@ static void dither_pixel(uint8_t *pixel)
   else if (pvalue < 0)
     pvalue = 0;
 
-  memmove(error, error + 1, (SIZE - 1) * sizeof error[0]);    /* shift queue */
+  memmove(error, error + 1, (SIZE - 1) * sizeof error[0]);    // shift queue
   error[SIZE - 1] = *pixel - (uint8_t)(pvalue + 0.5);
   *pixel = (uint8_t)(pvalue + 0.5);
 }
 
 static void move(direction_t direction)
 {
-  /* dither the current pixel */
+  // dither the current pixel
   if (cur_x >= 0 && cur_x < img_width &&
       cur_y >= 0 && cur_y < img_height)
     dither_pixel(img_ptr);
 
-  /* move to the next pixel */
+  // move to the next pixel
   switch (direction) {
   case LEFT:
     --cur_x;
@@ -86,7 +102,7 @@ static void move(direction_t direction)
     ++cur_y;
     img_ptr += img_width * img_bytes;
     break;
-  } /* switch */
+  }
 }
 
 void hilbert_level(int level, direction_t direction)
@@ -113,7 +129,7 @@ void hilbert_level(int level, direction_t direction)
       move(LEFT);
       move(DOWN);
       break;
-    } /* switch */
+    }
   }
   else {
     switch (direction) {
@@ -157,18 +173,18 @@ void hilbert_level(int level, direction_t direction)
   }
 }
 
+// substitutes "log2(n)", which is apparently not available on BSD, OS X
 inline double priv_log2(double n) {
   return log(n) / log(2);
-  //return log2(n);
 }
 
-void Riemersma(uint8_t *image, int width, int height, int shades, int bytes)
+void Riemersma(uint8_t* image, int width, int height, int shades, int bytes)
 {
   img_width = width;
   img_height = height;
   img_bytes = bytes;
   
-  /* determine the required order of the Hilbert curve */
+  // determine the required order of the Hilbert curve
   const int size = width > height ? width : height;
   
   for (int ch = 0; ch < bytes; ++ch) {
@@ -177,7 +193,7 @@ void Riemersma(uint8_t *image, int width, int height, int shades, int bytes)
       ++level;
     
     init_weights (weights, SIZE,MAX);
-    img_factor = (float) (shades - 1) / (float) 255;
+    img_factor = (-1.0 + shades) / 255.0;
     
     cur_x = 0;
     cur_y = 0;
@@ -190,4 +206,3 @@ void Riemersma(uint8_t *image, int width, int height, int shades, int bytes)
     move(NONE);
   }
 }
-
