058cf2348965090d34cd9118d3fe607591bf94ac
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2008-12-18 16:54:53 +0000

    * another accurance improvement method

--- lib/deskew.hh
+++ lib/deskew.hh
@@ -1,3 +1,21 @@
+/*
+ * Page boundary detection for auto-crop and de-skew.
+ * Copyright (C) 2006 - 2008 René Reb, ExactCODE GmbH
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
+
 #ifndef __DESKEW_HH
 #define __DESKEW_HH
 
@@ -6,7 +24,8 @@ struct deskew_rect {
   double x_back, y_back;
 };
 
-deskew_rect deskewParameters (Image& image, int background_lines);
-bool deskew (Image& image, int background_lines);
+deskew_rect deskewParameters (Image& image, int background_lines,
+			      bool from_bottom);
+bool deskew (Image& image, int background_lines, bool from_bottom = true);
 
 #endif
