1e6ae9e088468c7defc01b03f111883cd53e3d39
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2009-03-19 20:40:21 +0000

    * guard lib/Contours.hh for multi-inclusion

--- trunk/lib/Contours.cc
+++ trunk/lib/Contours.cc
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007 Valentin Ziegler, ExactCODE GmbH Germany.
- *               2008 Rene Rebe, ExactCODE GmbH Germany.
+ *               2008 - 2009 Rene Rebe, ExactCODE GmbH Germany.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
