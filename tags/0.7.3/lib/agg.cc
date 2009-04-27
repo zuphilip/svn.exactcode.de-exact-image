72b00e84dfd1937dc488739107afc1720c15444b
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2009-04-27 18:08:34 +0000

    * tagged 0.7.3

---
+++ tags/0.7.3/lib/agg.cc
@@ -0,0 +1,19 @@
+/*
+ * Agg to ExactImage bridge.
+ * Copyright (C) 2007 René Rebe
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+
+#include "agg.hh"
+#include "Image.hh"
+
+// the big methods later go here, ...
