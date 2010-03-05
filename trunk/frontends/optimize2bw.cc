62b4d1d61c4b837348748255295d6cc1f424544b
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2010-03-05 14:06:28 +0000

    * improved optimize2bw to work on multiple input and output files

--- trunk/frontends/optimize2bw.cc
+++ trunk/frontends/optimize2bw.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 - 2009 René Rebe, ExactCODE GmbH
+ * Copyright (C) 2005 - 2010 René Rebe, ExactCODE GmbH
  *           (C) 2005, 2006 Archivista GmbH, CH-8042 Zuerich
  * 
  * This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,8 @@
 #include <math.h>
 
 #include <iostream>
-#include <iomanip>
+#include <fstream>
+#include <limits>
 
 #include "ArgumentList.hh"
 
@@ -40,9 +41,9 @@ int main (int argc, char* argv[])
   Argument<bool> arg_help ("", "help",
 			   "display this help text and exit");
   Argument<std::string> arg_input ("i", "input", "input file",
-                                   1, 1);
+                                   1, std::numeric_limits<int>::max());
   Argument<std::string> arg_output ("o", "output", "output file",
-				    1, 1);
+				    1, std::numeric_limits<int>::max());
   Argument<int> arg_low ("l", "low",
 			 "low normalization value", 0, 0, 1);
   Argument<int> arg_high ("h", "high",
@@ -80,7 +81,7 @@ int main (int argc, char* argv[])
   if (!arglist.Read (argc, argv) || arg_help.Get() == true)
     {
       std::cerr << "Color / Gray image to Bi-level optimizer"
-                <<  " - Copyright 2005 - 2008 by René Rebe" << std::endl
+                <<  " - Copyright 2005 - 2010 by René Rebe" << std::endl
                 << "Usage:" << std::endl;
       
       arglist.Usage (std::cerr);
@@ -88,102 +89,107 @@ int main (int argc, char* argv[])
     }
   
   Image image;
-  if (!ImageCodec::Read (arg_input.Get(), image)) {
-    std::cerr << "Error reading input file." << std::endl;
-    return 1;
-  }
-  
-  int low = 0;
-  int high = 0;
-  int sloppy_threshold = 0;
-  int radius = 3;
-  double sd = 2.1;
-  
-  if (arg_low.Get() != 0) {
-    low = arg_low.Get();
-    std::cerr << "Low value overwritten: " << low << std::endl;
-  }
-  
-  if (arg_high.Get() != 0) {
-    high = arg_high.Get();
-    std::cerr << "High value overwritten: " << high << std::endl;
-  }
-  
-  if (arg_radius.Get() != 0) {
-    radius = arg_radius.Get();
-    std::cerr << "Radius: " << radius << std::endl;
-  }
-  
-  if (arg_sd.Get() != 0) {
-    sd = arg_sd.Get();
-    std::cerr << "SD overwritten: " << sd << std::endl;
-  }
-  
-  // convert to 1-bit (threshold)
-  int threshold = 0;
-  
-  if (arg_threshold.Get() != 0) {
-    threshold = arg_threshold.Get();
-    std::cerr << "Threshold: " << threshold << std::endl;
-  }
-  
-  optimize2bw (image, low, high, threshold, sloppy_threshold, radius, sd);
-
-  // scale image using interpolation
-  double scale = arg_scale.Get ();
-  int dpi = arg_dpi.Get ();
-  
-  if (scale != 0.0 && dpi != 0) {
-    std::cerr << "DPI and scale argument must not be specified at once!" << std::endl;
-    return 1;
-  }
-  
-  if (dpi != 0) {
-    if (image.resolutionX() == 0)
-      image.setResolution(image.resolutionY(), image.resolutionY());
-    
-    if (image.resolutionX() == 0) {
-      std::cerr << "Image does not include DPI information!" << std::endl;
-      return 1;
-    }
-    
-    scale = (double)(dpi) / image.resolutionX();
-  }
-
-  if (scale < 0.0) {
-    std::cerr << "Scale must not be negativ!" << std::endl;
-    return 1;
-  }
-  
-  std::cerr << "Scale: " << scale << std::endl;
-  
-  if (scale > 0.0)
+  for (int f = 0; f < arg_input.Size(); ++f)
     {
-      if (scale < 1.0)
-	box_scale (image, scale, scale);
-      else
-	bilinear_scale (image, scale, scale);
+      for (int n = 1, i = 0; i < n; ++i)
+	{
+	  int ret = ImageCodec::Read(arg_input.Get(f), image, "", i);
+	  if (!ret) {
+	    std::cerr << "Error reading input file." << std::endl;
+	    return 1;
+	  }
+	  if (i == 0) n = ret;
+	  
+	  int low = 0;
+	  int high = 0;
+	  int sloppy_threshold = 0;
+	  int radius = 3;
+	  double sd = 2.1;
+	  
+	  if (arg_low.Get() != 0) {
+	    low = arg_low.Get();
+	    std::cerr << "Low value overwritten: " << low << std::endl;
+	  }
+	  
+	  if (arg_high.Get() != 0) {
+	    high = arg_high.Get();
+	    std::cerr << "High value overwritten: " << high << std::endl;
+	  }
+	  
+	  if (arg_radius.Get() != 0) {
+	    radius = arg_radius.Get();
+	    std::cerr << "Radius: " << radius << std::endl;
+	  }
+	  
+	  if (arg_sd.Get() != 0) {
+	    sd = arg_sd.Get();
+	    std::cerr << "SD overwritten: " << sd << std::endl;
+	  }
+	  
+	  // convert to 1-bit (threshold)
+	  int threshold = 0;
+	  if (arg_threshold.Get() != 0) {
+	    threshold = arg_threshold.Get();
+	    std::cerr << "Threshold: " << threshold << std::endl;
+	  }
+	  
+	  optimize2bw (image, low, high, threshold, sloppy_threshold, radius, sd);
+	  
+	  // scale image using interpolation
+	  double scale = arg_scale.Get ();
+	  int dpi = arg_dpi.Get ();
+	  
+	  if (scale != 0.0 && dpi != 0) {
+	    std::cerr << "DPI and scale argument must not be specified at once!" << std::endl;
+	    return 1;
+	  }
+	  
+	  if (dpi != 0) {
+	    if (image.resolutionX() == 0)
+	      image.setResolution(image.resolutionY(), image.resolutionY());
+	    
+	    if (image.resolutionX() == 0) {
+	      std::cerr << "Image does not include DPI information!" << std::endl;
+	      return 1;
+	    }
+	    
+	    scale = (double)(dpi) / image.resolutionX();
+	  }
+	  
+	  if (scale < 0.0) {
+	    std::cerr << "Scale must not be negativ!" << std::endl;
+	    return 1;
+	  }
+	  
+	  if (scale > 0.0)
+	    {
+	      if (scale < 1.0)
+		box_scale (image, scale, scale);
+	      else
+		bilinear_scale (image, scale, scale);
+	    }
+	  
+	  if (arg_threshold.Get() == 0)
+	    threshold = 200;
+	  
+	  // denoise? only if more than 1bps in the source image
+	  if (arg_denoise.Get() && image.bps > 1)
+	    {
+	      colorspace_gray8_threshold (image, threshold);
+	      colorspace_gray8_denoise_neighbours (image);
+	      colorspace_gray8_to_gray1 (image);
+	    }
+	  else
+	    {
+	      if (image.bps > 1)
+		colorspace_gray8_to_gray1 (image, threshold);
+	    }
+	  
+	  if (!ImageCodec::Write(arg_output.Get(f), image)) {
+	    std::cerr << "Error writing output file." << std::endl;
+	    return 1;
+	  }
+	}
     }
-  
-  if (arg_threshold.Get() == 0)
-    threshold = 200;
-  
-  // denoise? only if more than 1bps in the source image
-  if (arg_denoise.Get() && image.bps > 1)
-    {
-      colorspace_gray8_threshold (image, threshold);
-      colorspace_gray8_denoise_neighbours (image);
-      colorspace_gray8_to_gray1 (image);
-    }
-  else
-    {
-      if (image.bps > 1)
-	colorspace_gray8_to_gray1 (image, threshold);
-    }
-  
-  if (!ImageCodec::Write(arg_output.Get(), image)) {
-    std::cerr << "Error writing output file." << std::endl;
-    return 1;
-  }
   return 0;
 }
