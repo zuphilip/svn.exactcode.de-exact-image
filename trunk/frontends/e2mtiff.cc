a5ce798fbaa31d725463badd8995c21de73b8cec
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2010-03-05 13:43:53 +0000

    * extend multi-image suport in e2mtiff: 	* use the new codec methods (no custom C, libtiff state "hack") 	* read all imagess from the input file's codec

--- trunk/frontends/e2mtiff.cc
+++ trunk/frontends/e2mtiff.cc
@@ -1,6 +1,6 @@
 /*
  * The ExactImage library's any to multi-page TIFF converted
- * Copyright (C) 2008 René Rebe
+ * Copyright (C) 2008 - 2010 René Rebe
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,12 +16,7 @@
 #include <math.h>
 
 #include <iostream>
-#include <iomanip>
-#include <map>
-#include <cctype>
-
-#include "tiffio.h"
-#include "tiff.hh"
+#include <fstream>
 
 #include "ArgumentList.hh"
 #include "Codecs.hh"
@@ -47,7 +42,7 @@ int main (int argc, char* argv[])
       arglist.Residuals().empty())
     {
       std::cerr << "any to multi-page TIFF convert" << std::endl
-                <<  "    - Copyright 2008 by René Rebe, ExactCODE" << std::endl
+                <<  "    - Copyright 2008 - 2010 by René Rebe, ExactCODE" << std::endl
                 << "Usage:" << std::endl;
       
       arglist.Usage (std::cerr);
@@ -56,8 +51,12 @@ int main (int argc, char* argv[])
   
   int errors = 0;
   int tiff_page = 1;
-  TIFF* tiff_context = TIFFOpen (arg_output.Get().c_str(), "w");
-  
+  std::fstream stream(arg_output.Get().c_str(), std::ios::in | std::ios::out | std::ios::trunc);
+  ImageCodec* codec = ImageCodec::MultiWrite(&stream, "tiff", ImageCodec::getExtension(arg_output.Get()));
+  if(!codec) {
+    std::cerr << "Error writing file: " << arg_output.Get() << std::endl;
+    return 1;
+  }
   Image image;
   
   const std::vector<std::string>& filenames = arglist.Residuals();
@@ -65,19 +64,24 @@ int main (int argc, char* argv[])
        file != filenames.end ();
        ++file)
     {
-      if (!ImageCodec::Read (*file, image)) {
-	std::cerr << "Error reading " << *file << std::endl;
-	++errors;
-	continue;
-      }
-      else {
-	// std::cerr << "adding [" << tiff_page << "] " << *file << std::endl;
-	TIFCodec::writeImageImpl (tiff_context, image, ""/*compression*/, tiff_page++);
+      int n = 1;
+      for (int i = 0; i < n; ++i)
+      {
+        int ret = ImageCodec::Read (*file, image, "", i);
+	if (ret == 0) {
+	  std::cerr << "Error reading " << *file << std::endl;
+	  ++errors;
+	  break;
+        }
+        else {
+	  if (i == 0) n = ret;
+	  // std::cerr << "adding [" << tiff_page << "] " << *file << std::endl;
+	  codec->Write (image, 75, ""/*compression*/, tiff_page++);
+        }
       }
     }
   
-  TIFFClose (tiff_context);
-  tiff_context = NULL;
+  delete(codec); codec = 0;
 
   return errors;
 }
