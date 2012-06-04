103f990334aea64c25ff0365424f6936d717ee06
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2012-06-04 10:30:02 +0000

    * added 10 arg codegen

--- lib/ImageIterator2.hh
+++ lib/ImageIterator2.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 - 2011 René Rebe, ExactCODE GmbH Germany.
+ * Copyright (C) 2008 - 2012 René Rebe, ExactCODE GmbH Germany.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1076,6 +1076,49 @@ void codegen (T1& a1, T2& a2, T3& a3, T4& a4,
   else if (a1.bps == 1) {
     ALGO <bit_iterator<1> > a;
     a (a1, a2, a3, a4, a5, a6, a7);
+  }
+}
+
+template <template <typename T> class ALGO,
+class T1, class T2, class T3, class T4,
+class T5, class T6, class T7, class T8,
+class T9, class T10>
+void codegen (T1& a1, T2& a2, T3& a3, T4& a4,
+	      T5& a5, T6& a6, T7& a7, T8& a8,
+	      T9& a9, T10& a10)
+{
+  if (a1.spp == 3) {
+    if (a1.bps == 8) {
+      ALGO <rgb_iterator> a;
+      a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+    } else {
+      ALGO <rgb16_iterator> a;
+      a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+    }
+  }
+  else if (a1.spp == 4 && a1.bps == 8) {
+    ALGO <rgba_iterator> a;
+    a (a1, a2, a3, a5, a5, a6, a7, a8, a9, a10);
+  }
+  else if (a1.bps == 16) {
+    ALGO <gray16_iterator> a;
+    a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+  }
+  else if (a1.bps == 8) {
+    ALGO <gray_iterator> a;
+    a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+  }
+  else if (a1.bps == 4) {
+    ALGO <bit_iterator<4> > a;
+    a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+  }
+  else if (a1.bps == 2) {
+    ALGO <bit_iterator<2> > a;
+    a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
+  }
+  else if (a1.bps == 1) {
+    ALGO <bit_iterator<1> > a;
+    a (a1, a2, a3, a4, a5, a6, a7, a8, a9, a10);
   }
 }
 
