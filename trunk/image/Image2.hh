3c8230c3363790421cfc9255660add45c6a6a257
rene <rene@cfa9e8bd-72fe-0310-85ed-f28a7036cb6a>
2016-06-21 15:43:40 +0000

    * switch to the stdint.h the other alien compilier prefers, ...

--- trunk/image/Image2.hh
+++ trunk/image/Image2.hh
@@ -1,7 +1,25 @@
+/*
+ * The Plain Old Data encapsulation of pixel, raster data.
+ * Copyright (C) 2005 - 2016 Ren\xc3\xa9 Rebe
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2. A copy of the GNU General
+ * Public License can be found in the file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANT-
+ * ABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * Alternatively, commercial licensing options are available from the
+ * copyright holder ExactCODE GmbH Germany.
+ */
+
 #ifndef IMAGE2_HH
 #define IMAGE2_HH
 
-#include <inttypes.h>
+#include <stdint.h>
 
 // the more optimizing next-gen Image::iterator DRAFT
 
